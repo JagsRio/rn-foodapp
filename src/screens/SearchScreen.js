8cf475a48958e33d1d47e4b2bc95111aeadf8423
JagsRio <jagadeswar.tanguturi2147@my.riohondo.edu>
2025-09-04 16:08:42 -0700

    Working on Axios

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,19 +1,41 @@
 import react, { useState } from 'react';
 import {Text, View, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
+    const [errMsg, setErrMessage] = useState('');
+
+    const searchApi = async () => {
+        try {
+            const response = await yelp.get('/search', {
+                params:{
+                    limit: 25,
+                    term : term,
+                    location: 'fullerton',
+                }
+            });
+            setResults(response.data.businesses);   
+        } catch (error) {
+            setErrMessage("Something went wrong");
+        }
+    }
 
     return (
         <View>
             <SearchBar 
                 term={term} 
                 onTermChange={ (newTerm)=> setTerm(newTerm) }
-                onTermSubmit={ ()=> console.log('Term was submitted') }
+                onTermSubmit={ ()=> searchApi() }
             />
-            <Text>Search - term = {term} </Text>
+            
+            return {errMsg.length > 0}
+            ? <Text>{errMsg}</Text>
+            : null
+
+            <Text>Search found = {results.length} results </Text>
         </View>
     )
 }
